4cc6b6e6f700bc142690dd8234f36c39466ccb0c
lifeeeDEV <137718464+lifeeeDEV@users.noreply.github.com>
2024-05-12 17:50:56 +0200

    Update page.tsx

--- app/(root)/communities/[id]/page.tsx
+++ app/(root)/communities/[id]/page.tsx
@@ -10,14 +10,14 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import { fetchCommunityDetails } from "@/lib/actions/community.actions";
 
-async function Page({ params }: { params: { id: string } }) {
+async function Page({ params }) {
   const user = await currentUser();
   if (!user) return null;
 
-  let communityDetails = await fetchCommunityDetails(params.id);
+  const communityDetails = await fetchCommunityDetails(params.id);
 
   // Sortieren der Threads nach dem 'createdAt' Datum, falls sie nicht bereits sortiert sind
-  if (communityDetails.threads) {
+  if (communityDetails && communityDetails.threads) {
     communityDetails.threads.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
   }
 
@@ -30,25 +30,24 @@ async function Page({ params }: { params: { id: string } }) {
         username={communityDetails.username}
         imgUrl={communityDetails.image}
         bio={communityDetails.bio}
-        type='Community'
+        type="Community"
       />
 
-      <div className='mt-9'>
-        <Tabs defaultValue='threads' className='w-full'>
-          <TabsList className='tab'>
+      <div className="mt-9">
+        <Tabs defaultValue="threads" className="w-full">
+          <TabsList className="tab">
             {communityTabs.map((tab) => (
-              <TabsTrigger key={tab.label} value={tab.value} className='tab'>
+              <TabsTrigger key={tab.label} value={tab.value} className="tab">
                 <Image
                   src={tab.icon}
                   alt={tab.label}
                   width={24}
                   height={24}
-                  className='object-contain'
+                  className="object-contain"
                 />
-                <p className='max-sm:hidden'>{tab.label}</p>
-
+                <p className="max-sm:hidden">{tab.label}</p>
                 {tab.label === "Threads" && (
-                  <p className='ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2'>
+                  <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2">
                     {communityDetails.threads.length}
                   </p>
                 )}
@@ -56,35 +55,35 @@ async function Page({ params }: { params: { id: string } }) {
             ))}
           </TabsList>
 
-          <TabsContent value='threads' className='w-full text-light-1'>
+          <TabsContent value="threads" className="w-full text-light-1">
             <ThreadsTab
               currentUserId={user.id}
               accountId={communityDetails._id}
-              accountType='Community'
+              accountType="Community"
               threads={communityDetails.threads}
             />
           </TabsContent>
 
-          <TabsContent value='members' className='mt-9 w-full text-light-1'>
-            <section className='mt-9 flex flex-col gap-10'>
-              {communityDetails.members.map((member: any) => (
+          <TabsContent value="members" className="mt-9 w-full text-light-1">
+            <section className="mt-9 flex flex-col gap-10">
+              {communityDetails.members.map((member) => (
                 <UserCard
                   key={member.id}
                   id={member.id}
                   name={member.name}
                   username={member.username}
                   imgUrl={member.image}
-                  personType='User'
-                ))
-              }
+                  personType="User"
+                />
+              ))}
             </section>
           </TabsContent>
 
-          <TabsContent value='requests' className='w-full text-light-1'>
+          <TabsContent value="requests" className="w-full text-light-1">
             <ThreadsTab
               currentUserId={user.id}
               accountId={communityDetails._id}
-              accountType='Community'
+              accountType="Community"
             />
           </TabsContent>
         </Tabs>
